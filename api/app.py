6a32ed290ceaf69c1e1829ef3dd89a462068957a
Nikko <nickouchiha5@gmail.com>
2023-06-18 15:25:30 +0800

    ADDED

--- api/app.py
+++ api/app.py
@@ -3,7 +3,7 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'
+app.secret_key = secrets.token_hex(16)
 
 # Spotify API credentials
 CLIENT_ID = 'ba6a268d7e264ea899acac717e78ac09'
@@ -31,14 +31,30 @@ def login():
     auth_url = sp_oauth.get_authorize_url()
     return redirect(auth_url)
 
-# Callback route after successful login
+# Callback route
 @app.route('/callback')
 def callback():
-    session.clear()
+    # Get authorization code from the request
     code = request.args.get('code')
+
+    # Exchange authorization code for access token
     token_info = sp_oauth.get_access_token(code)
-    session['token_info'] = token_info
-    return redirect(url_for('search'))
+    access_token = token_info['access_token']
+
+    # Set the access token for the Spotify client
+    spotify.client_credentials_manager.token_info = token_info
+
+    # Redirect to the results page
+    return redirect(url_for('results'))
+
+# Results route
+@app.route('/results')
+def results():
+    query = request.args.get('query')
+    results = spotify.search(q=query, type='track', limit=10)
+    tracks = results['tracks']['items']
+    return render_template('results.html', tracks=tracks)
+
 
 # Search route
 @app.route('/search', methods=['GET', 'POST'])
